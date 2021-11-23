44ea53eabe89a35f2f0a0c33985ea37b4b2af037
Ramith Laksara <ramith.laksara@gmail.com>
2021-11-23 09:13:29 +0530

    batsmen class created

--- src/Batsmen.java
+++ src/Batsmen.java
@@ -1,21 +1,45 @@
 public class Batsmen {
-
-    public int balls;
+    /*
+    Setting attributes
+    faced balls = 0
+    strike rate = 0
+    no of sixes = 0
+    number of fours = 0
+    player score = 0
+    player name = ""
+     */
+    public int facedBalls;
+    public int playerScore;
+    public double strikeRate;
+    public int noSixes;
+    public int noFours;
     public String name;
 
-    public Batsmen(String name){
-        balls = 0;
+    public Batsmen(String name) {
+        this.facedBalls = 0;
+        this.playerScore = 0;
+        this.strikeRate = 0;
+        this.noSixes = 0;
+        this.noFours = 0;
         this.name = name;
     }
 
-    public int countBalls(){
-
-        balls += 1;
-        System.out.println(balls);
-        return balls;
-
+    public int countBalls() {
+        facedBalls += 1;
+        return facedBalls;
     }
-    public static double myMethod(double score, double balls) {
-        return (score/balls) * 100;
+    public int countSix() {
+        noSixes += 1;
+        return noSixes;
     }
+    public int countFou() {
+        noFours += 1;
+        return noFours;
+    }
+    public int countScore(int score) {
+        playerScore = playerScore + score;
+        return playerScore;
+    }
+
+
 }
